e8b5d6ed9cf5bffcd515b1b5c1aee77ccd171d66
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-06-27 23:27:45 -0700

    added all channel route

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
 from .config import Configuration
-from .routes import session
+from .routes import session, channel
 from .models import db
 
 
@@ -10,5 +10,6 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_object(Configuration)
 app.register_blueprint(session.bp)
+app.register_blueprint(channel.bp)
 db.init_app(app)
 Migrate(app, db)
---
+++ app/routes/channel.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request
+from app.models import User, Container, db
+from ..config import Configuration
+from ..util import token_required
+
+bp = Blueprint('channel', __name__, url_prefix='/channel')
+
+
+# RELOAD
+@bp.route('/all')
+@token_required
+def get_channels(current_user):
+    channels = Container.query.filter_by(is_channel=True).all()
+    print(channels)
+    returnchannels = dict((channel.id, {
+        'title': channel.title,
+        'topic': channel.topic,
+    }) for channel in channels)
+    return {
+        'data': returnchannels
+    }
